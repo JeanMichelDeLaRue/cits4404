c78a56a703f1c1b34b747fdcd9c509aa1d13611d
myxie <21298244@student.uwa.edu.au>
2017-10-02 18:29:18 +0800

    Very initial additions

---
+++ grid.py
@@ -0,0 +1,16 @@
+"""
+Python implementation of Ant Colony Optimisation (ACO) meta-heuristic, for course requirements of CITS4404 @ University of Western Australia
+"""
+
+class Target(object):
+
+	def __init__(self,xinit, yinit):
+
+
+
+class Grid(object):
+
+	def __init__(self, xmax=500, ymax=500): 
+		self._gridx = xmax
+
+
