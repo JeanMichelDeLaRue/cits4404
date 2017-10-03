7329d97a1cc52c1f5bbb4910ea011392ce758961
myxie <21298244@student.uwa.edu.au>
2017-10-03 09:42:28 +0800

    Working on running an iteration

--- grid.py
+++ grid.py
@@ -2,6 +2,7 @@
 Python implementation of Ant Colony Optimisation (ACO) meta-heuristic, for course requirements of CITS4404 @ University of Western Australia
 """
 import random 
+import networkx as nx
 class Target(object):
 
     def __init__(self,tid, xinit, yinit):
@@ -19,7 +20,7 @@ class Target(object):
     def velocity(self):
         return self._velocity
 
-    def update_position(self):
+    def update_position(self,x,y,gridx,gridy,iteration):
         """
         We need to update the position of the target after an iteration
         """
@@ -38,11 +39,16 @@ class Grid(object):
         else: 
             self._target_list = []
 
+    def __len__(self):
+        return len(self._target_list)
+
+    def size(self):
+        return (self._gridx,self._gridy)
+
     def add_target_to_grid(self,target):
         self._target_list.append(target)
     
-    def update_target_position(self,target): 
-       return -1  
+        
     def distance(self, t1, t2):
         return -1 
 
@@ -58,8 +64,28 @@ class Grid(object):
 class Ant(object): 
 
     def __init__(self, target):
-        self.rand_target = target
-        self.solution = None # This is graph
+        self._target = target
+        self._solution = nx.Graph() # This is graph
+        self._previous = None
+
+    def __repr__(self):
+        return self._solution.nodes()
+
+    def current_position(self):
+        return self._target.current_position()
+
+    def get_solution(self):
+        return self._solution  
+    # def 
+
+    def update_solution(self, new_node,distance=None):
+        if not self._previous:
+            self._previous = new_node
+            self._solution.add_node(new_node)
+        else:
+            self._solution.add_node(new_node)
+            self._solution.add_edge(self._previous,new_node,weight=distance)
+            self._previous = new_node
 
     def fitness_selection(self):
         return -1
@@ -69,31 +95,60 @@ class AntColony(object):
     def __init__(self,grid,num_ants=25,alpha=1, beta=0.1):
         self._grid = grid 
         self._iteration = 0
+        self._ants = num_ants
         self._colony = []
         self._alpha = alpha
         self._beta = beta
-        self._pheromone = 0
+        # self._pheromone = 0
 
 
-    def _init_ants(self, num_ants):
+    def _init_ants(self):
         """
         This function initialises ants in the colony 
         """
-        for i in range(0,num_ants):
-            target = self.grid 
+        grid_size = len(self._grid)
+        for i in range(0,self._ants):
+            index = random.randint(0,grid_size-1)
+            target = grid.targets()[index]
+
+            ant = Ant(target)   
+            self._colony.append(ant)    
         return -1 
 
     def grid_state(self):
         return self._grid.state()
 
     def run(self):
-       """
-       Running the simulations follows the following structure:
-       1. Each ant constructs a solution
-       2. Ants lay pheremones on the solution path
-       3. After all pheremones are laid, we evaporate according to the evaporation rate
-       """ 
-       return -1  
+        if not self._colony:
+            self._init_ants()
+
+        for ant in self._colony:
+            max_solution = 0 
+            print ant.current_position()
+            target_list = self._grid.targets()
+            current_solution = ant.get_solution()
+            previous_node = None
+            count = 1 
+            while len(current_solution) < len(self._grid):
+                """
+                This is where the pheremone/path selection occurs - not implemented yet
+                """ 
+                index = random.randint(0,len(self._grid)-1)
+                target = target_list[index]                
+                gridx, gridy = self._grid.size()
+                x,y = target.current_position()
+                target.update_position(x,y,gridx,gridy,count)
+                if target not in current_solution:
+                    ant.update_solution(target)
+                count = count + 1    
+
+        """
+        Running the simulations follows the following structure:
+        1. Each ant constructs a solution
+        2. Ants lay pheremones on the solution path
+        3. After all pheremones are laid, we evaporate according to the evaporation rate
+        """ 
+        return -1  
 
 
 if __name__ == "__main__": 
@@ -102,11 +157,11 @@ if __name__ == "__main__":
     targetC = Target("C",7,8)
 
     grid = Grid(250,250,[targetA,targetB,targetC])
-    grid.state()    
+    # grid.state()    
 
     aco = AntColony(grid,10)
-    aco.grid_state()
-
+    # aco.grid_state()
+    aco.run()
     """
     TODO:
     Have the Ant colony 'run' over n iterations
